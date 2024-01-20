56d0c5d349ba037c5227a4cbdf53609acc3abb6d
Talita-S <talitassantos2015@gmail.com>
2024-01-20 20:33:26 -0300

    feat: adding message component for user feedback

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MessageComponent } from './shared/message/message.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MessageComponent],
   imports: [BrowserModule, ReactiveFormsModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
