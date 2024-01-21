862d79e556debdbaa169a88770eb46e6b1132fe5
Talita-S <talitassantos2015@gmail.com>
2024-01-20 21:58:55 -0300

    test: adding test scenarios for addTask function

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpResponse,
+} from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { of, throwError } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(() =>
@@ -30,5 +35,37 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('a')?.textContent).toContain(
       'GERENCIAMENTO DE TAREFAS'
     );
+  });
+
+  it('should add a task successfully', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const httpClient = TestBed.inject(HttpClient);
+    spyOn(httpClient, 'post').and.returnValue(of({} as HttpResponse<Response>));
+
+    app.description.setValue('Test Task');
+
+    app.addTask();
+
+    expect(httpClient.post).toHaveBeenCalled();
+    expect(app.openCollapse).toBe(false);
+    expect(app.showText).toBe(true);
+    expect(app.showAddSuccess).toBe(true);
+  });
+
+  it('should handle error when adding a task', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const httpClient = TestBed.inject(HttpClient);
+    spyOn(httpClient, 'post').and.returnValue(
+      throwError(() => new Error('error'))
+    );
+
+    app.description.setValue('Test Task');
+
+    app.addTask();
+
+    expect(httpClient.post).toHaveBeenCalled();
+    expect(app.showFailure).toBe(true);
   });
 });
